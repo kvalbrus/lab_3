b8aab518bb560e3d0424fec3ea9a4ec3a7e3d267
makkagru <hakobyanarthur401@gmail.com>
2021-11-02 21:17:40 +0300

    Input port

---
+++ 1-3.c
@@ -0,0 +1,10 @@
+struct position input_port() {
+	struct position port;
+	scanf("%d", &port.lat.degree);
+	scanf("%d", &port.lat.minute);
+	scanf("%d", &port.lat.second);
+	scanf("%d", &port.longitude.degree);
+	scanf("%d", &port.longitude.minute);
+	scanf("%d", &port.longitude.second);
+	return port;
+}
